596ad750b9486312dbfd1e9cebd3b378f16f0bbc
Victor Morales <victormoralestosto@hotmail.com>
2023-08-17 22:45:44 -0300

    adaptação para tablets finalizada

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,8 +3,10 @@ import './Banner.css';
 const Banner = () => {
     return (
         <section className='banner'>
-            <h1 className='banner__text'>Escute milhões de músicas e podcasts de graça.</h1>
-            <a className='banner__link' href="/">Baixe o Spotify Free</a>
+            <div className='banner__container'>
+                <h1 className='banner__text'>Escute milhões de músicas e podcasts de graça.</h1>
+                <a className='banner__link' href="/">Baixe o Spotify Free</a>
+            </div>
         </section>
     );
 }
